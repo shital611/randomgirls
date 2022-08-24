faa23c482fa1db070d17f8e837b9a8e152b65c45
shital <shitalsonawane51120@gmail.com>
2022-08-24 16:55:22 +0530

    done

--- src/models/winnerHistory.js
+++ src/models/winnerHistory.js
@@ -1,34 +1,29 @@
 const mongoose=require('mongoose')
 const winnerHistorySchema=new mongoose.Schema({
-    // ID:{
-    //     type:Number,
-    //     // required:true,
-    //     // unique:true
-    // },
+   
     PoolID:{
         type:Number,
-        // required:true,
-        unique:true
+        required:true,
+        // unique:true
     },
     Winner1UserID:{
         type:Number,
-        // required:true,
-        unique:true
+        required:true,
+        // unique:true
     },
     Winner2UserID:{
         type:Number,
-        // required:true,
-        unique:true
+        required:true,
+        // unique:true
     },
     Winner3UserID:{
         type:Number,
-        // required:true,
-        unique:true
+        required:true,
+        // unique:true
     } 
 }
 ,{
     versionKey: false
 })
-
 const winnerHistoryData=new mongoose.model("Winner",winnerHistorySchema)
 module.exports=winnerHistoryData
